9f6253828cf68d831bc28c5985b7f7642b6c3afd
Jurczuk <nika.jurczuk@gmail.com>
2021-09-16 13:14:05 +0200

    writing to pubsub

--- vpc-peer-net-count.py
+++ vpc-peer-net-count.py
@@ -4,9 +4,9 @@
 import googleapiclient.discovery
 from google.oauth2 import service_account
 import google.cloud.logging
+from google.cloud import pubsub_v1
 import os
 
-SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
 
 # SET YOUR OWN VARIABLES
 SERVICE_ACCOUNT_FILE = '<Path to your key file (in json) for service account>'
@@ -15,15 +15,20 @@ DIRECTION            = '<The direction of the exchanged routes - INCOMING or OUT
 NETWORK              = '<Name of the network for this request>'
 PROJECT_ID           = '<Project ID for this request>'
 REGION               = '<The region of the request>'
+TOPIC_PATH           = '<Path to your topic in PubSub>'
 
 # Sets environment variable
 try:
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = SERVICE_ACCOUNT_FILE
+    os.environ['GOOGLE_TOPIC_PATH'] = TOPIC_PATH
 except NameError:
     print('Variable does not exist')
 
+publisher = pubsub_v1.PublisherClient()
+SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
+
 credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 compute = googleapiclient.discovery.build('compute', 'v1', credentials=credentials)
 
@@ -39,11 +44,27 @@ log_name = "vpc-network-peering-logs"
 logger = client.logger(log_name)
 
 if 'items' in response:
-        text = f"Number of {DIRECTION} peered networks: {len(response['items'])}"
-        # Writes the log entry
-        logger.log_text(text, severity="INFO")
+    length = len(response['items'])
+    text = f"Number of {DIRECTION} peered networks: {length}"
+    # Writes the log entry
+    logger.log_text(text, severity="INFO")
+    text = text.encode('utf-8')
+    future = publisher.publish(TOPIC_PATH, text)  # message id
+    print(f'published message id {future.result()}')
+
+    if length > 50:
+        error = "Too many peered networks - max 50."
+        logger.log_text(error, severity="ERROR")
+        error = error.encode('utf-8')
+        future = publisher.publish(TOPIC_PATH, error)  # message id
+        print(f'published message id {future.result()}')
+        print("Logged: {}".format(error))
+
 else:
-        text = "No peered networks."
-        logger.log_text(text, severity="INFO")
+    text = "No peered networks."
+    logger.log_text(text, severity="INFO")
+    text = text.encode('utf-8')
+    future = publisher.publish(TOPIC_PATH, text)  # message id
+    print(f'published message id {future.result()}')
 
 print("Logged: {}".format(text))
