8a48ac0a72490f8266895c0773f4f7eaf30e1913
Jurczuk <nika.jurczuk@gmail.com>
2021-09-22 16:31:30 +0200

    change paths

--- delete-metric.py
+++ delete-metric.py
@@ -1,5 +1,5 @@
 from google.cloud import monitoring_v3
 
 client = monitoring_v3.MetricServiceClient()
-client.delete_metric_descriptor(name="projects/rational-moon-320316/metricDescriptors/custom.googleapis.com/number-of-peered-networks-metric")
-print("Deleted metric descriptor {}.".format("projects/rational-moon-320316/metricDescriptors/custom.googleapis.com/number-of-peered-networks-metric"))
+client.delete_metric_descriptor(name=descriptor_name)
+print("Deleted metric descriptor {}.".format(descriptor_name))
